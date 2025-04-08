82f6522f6dcc0b53823772ba966f6df42a226ce5
Milad704 <miladgobbo@gmail.com>
2025-04-08 10:49:00 -0700

    small changes, enemy area

--- script/enemy.gd
+++ script/enemy.gd
@@ -1,7 +1,9 @@
 extends CharacterBody2D
 const gravity = 60
 var health = 3
-
+var speed = 25
+var player_chase = false
+var player = null
 func _physics_process(delta: float) -> void:
 	velocity.y += gravity
 	move_and_slide()
@@ -11,4 +13,13 @@ func _physics_process(delta: float) -> void:
 
 
 func _on_dmgarea_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+
+func _on_detection_area_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+
+
+
+
+func _on_detection_area_body_exited(body: Node2D) -> void:
 	pass # Replace with function body.
