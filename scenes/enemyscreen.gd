ddb18a9b2bddecf8bfa653f43f67d405d1aa9340
Milad704 <miladgobbo@gmail.com>
2025-04-09 08:48:57 -0700

    enemy movement

--- scenes/enemyscreen.gd
+++ scenes/enemyscreen.gd
@@ -1,1 +1,12 @@
 extends CharacterBody2D
+var movespeed = 100
+const gravity = 60
+var health = 3
+
+func _physics_process(delta: float) -> void:
+	velocity = movespeed
+	move_and_slide()
+
+func _physics_process(delta: float) -> void:
+	velocity.y += gravity
+	move_and_slide()
