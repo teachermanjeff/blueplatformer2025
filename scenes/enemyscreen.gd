435026a6fd8f950e0c3304f076ba357a998c76ad
Milad704 <miladgobbo@gmail.com>
2025-04-14 08:58:57 -0700

    work on detecting enemy.

--- scenes/enemyscreen.gd
+++ scenes/enemyscreen.gd
@@ -2,12 +2,16 @@ extends CharacterBody2D
 var movespeed = 50
 const gravity = 60
 @export var anim: AnimatedSprite2D
-
+var player = null
+var player_chase = false
+var speed = 25
 
 func _ready():
 	pass
 	
 func _physics_process(delta: float) -> void:
+	if player_chase:
+		position += (player.position - position)/speed
 	velocity.x = movespeed
 	$Sprite2D.play("default")
 	move_and_slide()
@@ -33,4 +37,10 @@ func _on_hitbox_body_entered(body: Node2D) -> void:
 func _on_hurt_area_entered(area: Area2D) -> void:
 	area.queue_free()
 	queue_free()
+	pass # Replace with function body.
+
+
+func _on_detectionzone_body_entered(body: Node2D) -> void:
+	player = body
+	player_chase = true
 	pass # Replace with function body.
