8d1a9f5577a89796a3aacdddf0a5b3085bbc4716
Milad704 <miladgobbo@gmail.com>
2025-04-17 12:01:07 -0700

    enemy shooting

--- scenes/enemyscreen.gd
+++ scenes/enemyscreen.gd
@@ -6,18 +6,37 @@ var player = null
 var player_chase = false
 var speed = 25
 var dead = false
+var current_dir = "right"  # direction the character is facing
+var shoot = true
+var bullet = preload("res://enemy_bullet.tscn") # Drag & drop Bullet.tscn in the inspector
+@onready var gun_muzzle = $gun_muzzle  # Make sure you added a Marker2D called "GunMuzzle"
 
 func _ready():
 	pass
 	
 func _physics_process(delta: float) -> void:
+
 	if player_chase:
 		#velocity.x = movespeed
 		if player.position.x > position.x:
 			velocity.x += 2
 		if player.position.x < position.x:
 			velocity.x -= 2
-	
+			
+		#var Bullet = bullet.instantiate()
+		if shoot == true:
+			var new_bullet = bullet.instantiate()
+			new_bullet.global_position = gun_muzzle.global_position
+			get_parent().add_child(new_bullet)
+			if current_dir == "right":
+				new_bullet.direction = 1
+			else:
+				new_bullet.direction = -1
+			shoot = false
+			$Timer.start()
+
+
+		#get_tree().current_scene.add_child(bullet)
 	$Sprite2D.play("default")
 	move_and_slide()
 
@@ -55,4 +74,9 @@ func _on_detectionzone_body_entered(body: Node2D) -> void:
 		print("entered2")
 		player = body
 		player_chase = true
+	pass # Replace with function body.
+
+
+func _on_timer_timeout() -> void:
+	shoot = true
 	pass # Replace with function body.
