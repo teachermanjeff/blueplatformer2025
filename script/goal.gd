1a073c2c877896fcb740a3289bffb1645fdc61d0
The menu pricer <redx19764@gmail.com>
2025-05-06 11:58:41 -0700

    added a endscreen and edited the goal so that it works

--- script/goal.gd
+++ script/goal.gd
@@ -3,5 +3,5 @@ extends Area2D
 @export_file var nextlevel
 
 func _on_body_entered(body: CharacterBody2D) -> void:
-	get_tree().change_scene_to_file(nextlevel)
+	get_tree().change_scene_to_file("res://scenes/endscreen.tscn")
 	pass # Replace with function body.
